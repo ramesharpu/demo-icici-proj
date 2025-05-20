80d300e19d15f8d7a257f0dc905d53c93b89de11
Ramesh A <ramesharpu@gmail.com>
2025-05-20 15:21:48 +0530

    intial commit

---
+++ src/main/java/com/icicibank/demo_icici_proj/DemoIciciProjApplication.java
@@ -0,0 +1,21 @@
+package com.icicibank.demo_icici_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoIciciProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIciciProjApplication.class, args);
+	}
+	
+	@GetMapping("/")
+    public String hello() {
+      return String.format("<h1>Hello There!!! Welcome to my project</h1>");
+    }
+
+}
